a57cda4458bcd9fc2416f5ea62ca1b055698f5bf
CristianoFelipe <cristianofnunes97@gmail.com>
2022-04-28 10:43:33 -0300

    react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return (
+    <>
+    <h1 className="titulo">Home</h1>
+    <img src="https://i.imgur.com/jhua4N5.jpeg" alt="Imagem Tela Inicial" className="img"/>
+    </>
+ );
+}
+
+export default Home;
