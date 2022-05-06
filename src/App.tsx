947dfd3cdeac094f2c83b29d05de963106625202
CristianoFelipe <cristianofnunes97@gmail.com>
2022-05-06 10:35:12 -0300

    sistema de rotas login e home

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import React from "react";
-import Navbar from "./componentes/estaticos/navbar/Navbar";
-import Footer from "./componentes/estaticos/navbar/Navbar";
-import Home from "./paginas/home/Home";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
 import './App.css';
+import Login from './paginas/login/Login';
+
 
 
 function App() {
   return (
-    <>
+
+    <Router>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
       <Footer />
-
-
-    </>
+    </Router>
   );
+
 }
 
 export default App;
