8c9d16a327117533fa7f7125e16956267fb0a131
CristianoFelipe <cristianofnunes97@gmail.com>
2022-05-07 04:01:40 -0300

    criação login e cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
@@ -5,25 +6,27 @@ import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
 import Login from './paginas/login/Login';
-
-
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 function App() {
   return (
-
     <Router>
       <Navbar />
-      <div style={{ minHeight: '100vh' }}>
-        <Routes> // Antigo Switch
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </div>
+      <Routes>
+
+        <Route path="/" element={<Login  />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+
+      </Routes>
       <Footer />
     </Router>
-  );
 
+  );
 }
 
 export default App;
